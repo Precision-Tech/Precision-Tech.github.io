e4f861e49c3425650ea360e22e3dc5cda41bb583
WalterHummel <walter.hummel@rutgers.edu>
2016-01-18 23:46:40 -0500

    updating map

--- js/maps.js
+++ js/maps.js
@@ -2,15 +2,24 @@
 var map;
 function initMap() {
   var myLatLng = {lat: 39.596217, lng: -74.800530};
+  var useragent = navigator.userAgent;
 
   map = new google.maps.Map(document.getElementById('map'), {
     center: myLatLng,
-    zoom: 15
+    zoom: 15,
+    draggable: false
   });
 
   var marker = new google.maps.Marker({
     position: myLatLng,
     map: map,
-    title: 'Precision-Tech'
+    title: 'Precision-Tech',
   });
+
+  if (useragent.indexOf('iPhone') != -1 || useragent.indexOf('Android') != -1 ) {
+    map.setOptions({
+      draggable: false,
+      disableDefaultUI: true
+    });
+  }
 }
